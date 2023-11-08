4a941074f6eeb376ec138c98f7a2b5ae53837402
Fr4nk13d3vs <danielwilliamcss@gmail.com>
2023-11-08 10:02:33 -0300

    Version actualizada

--- app/src/main/java/com/example/sqlite/MainActivity.java
+++ app/src/main/java/com/example/sqlite/MainActivity.java
@@ -18,6 +18,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     EditText ID, Usuario, AreaUsuario;
     ListView Lista;
+    //Actualizacion de codigo
 
 
     @Override
